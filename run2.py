8d841ebd90a0d5a4b033719e07f17402808707b8
aehyj92 <76893021+aehyj92@users.noreply.github.com>
2023-04-05 14:55:32 +0900

    Create run2.py

---
+++ run2.py
@@ -0,0 +1,1 @@
+python train_and_evaluate_1.py --data_path rcc_1.csv --output_path results --model lgbm --n_folds 5 --random_state 42 --num_iter 100 --max_depth 50 --eval_metric auc --early_stopping_rounds 10 
